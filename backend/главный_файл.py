e9f747ec048ec111f204656f8542e29e006a17b1
armbdevelop <armanbenyaminyan12@gmail.com>
2025-04-01 12:53:43 +0300

    создание бд

--- backend/главный_файл.py
+++ backend/главный_файл.py
@@ -1,9 +1,18 @@
+import uvicorn
 from fastapi import FastAPI as ИнтерфейсПрограмногоПриложения
+from app.api.видеть import вайфайроутер as router
 
 ИПП = ИнтерфейсПрограмногоПриложения
 
 приложение = ИПП()
 
+приложение.include_router(router)
+
+
 @приложение.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+if __name__ == '__main__':
+    uvicorn.run('главный_файл:приложение', reload=True)
